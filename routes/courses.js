17357220043fc493235642be4ac843abedf50ae3
edenadiv <info@edenadiv.com>
2025-01-04 22:58:44 +0200

    Added register to a course

--- routes/courses.js
+++ routes/courses.js
@@ -6,7 +6,6 @@ const authenticateToken = require('../middlewares/authenticateToken');
 
 const router = express.Router();
 
-// Add a course (staff only)
 router.post(
     '/',
     authenticateToken('staff'),
@@ -30,18 +29,57 @@ router.post(
 
             res.status(201).json({ message: 'Course added successfully', course });
         } catch (error) {
-            next(error); // Pass error to centralized handler
+            next(error);
         }
     }
 );
 
-// View all courses
+router.post('/register', authenticateToken('student'), async (req, res, next) => {
+    const { courseId } = req.body;
+
+    try {
+        const student = await Student.findOne({ id: req.user.id });
+        const course = await Course.findOne({ id: courseId });
+
+        if (!course) {
+            return res.status(404).json({ message: 'Course not found' });
+        }
+
+        if (course.enrolledStudents.length >= course.maxStudents) {
+            return res.status(400).json({ message: 'Course is full' });
+        }
+
+        if (student.registeredCourses.includes(course._id)) {
+            return res.status(400).json({ message: 'Already registered for this course' });
+        }
+
+        const totalCredits = student.registeredCourses.reduce(async (total, courseId) => {
+            const course = await Course.findById(courseId);
+            return total + course.creditPoints;
+        }, 0);
+
+        if (totalCredits + course.creditPoints > 20) {
+            return res.status(400).json({ message: 'Exceeds 20 credit points. Drop a course to proceed.' });
+        }
+
+        student.registeredCourses.push(course._id);
+        course.enrolledStudents.push(student._id);
+
+        await student.save();
+        await course.save();
+
+        res.json({ message: 'Registered for course successfully', student });
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.get('/', authenticateToken(), async (req, res, next) => {
     try {
         const courses = await Course.find();
         res.json(courses);
     } catch (error) {
-        next(error); // Pass error to centralized handler
+        next(error);
     }
 });
 
